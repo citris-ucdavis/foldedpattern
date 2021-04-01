bf1143ae5af839592786f5b05c497fe6fb113da5
ahmct-trav <trav@ucdavis.edu>
2021-03-31 22:25:31 -0700

    foldedpattern base commit

---
+++ Assets/RobotAnimationControlerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RelatedObjects{
+    public class RobotAnimationControlerScript : MonoBehaviour
+    {
+        Animator myAnimator;
+        // Start is called before the first frame update
+        void Start()
+        {
+            //get the animator component
+            myAnimator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        //enter clap state
+        public void MakeClap()
+        {
+            //reset cheering trigger to ensure it is false
+            myAnimator.ResetTrigger("Clap");
+            //trigger clapping
+            myAnimator.SetTrigger("Clap");
+        }
+    }
+}
